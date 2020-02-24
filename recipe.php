0b1503238c129a728d443bcc28aa470d5a7cd137
Iliyan <IDIliev18@codingburgas.bg>
2020-02-24 18:29:07 +0200

    add <br>

--- recipe.php
+++ recipe.php
@@ -12,11 +12,11 @@ include("dbConnect.php");
 <body>
    
     <form action="process.php" method="post" name="user">
-        Name: <input type="text" name="name" value=""><br>
-        Email:  <input type="email" name="email" value =""><br>
-        Recipe Name: <input type="text"name="rname">
-        Ingradients: <textarea name="Ingradients"></textarea>
-        How to make: <textarea name="HowToMake"></textarea>
+        Name: <input type="text" name="name" value="" required><br>
+        Email:  <input type="email" name="email" value ="" required><br>
+        Recipe Name: <input type="text"name="rname" required><br>       
+        Ingradients: <textarea name="Ingradients" required></textarea><br>
+        How to make: <textarea name="HowToMake" required></textarea><br>
         <input type="submit" value="Submit">
     </form>
     <a href="index.html">Riba</a>
