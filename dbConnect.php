8cc14ef9f82ede13b6292611c838b617534f3f55
Iliyan <IDIliev18@codingburgas.bg>
2020-02-26 15:29:36 +0200

    Merge Conflict

--- dbConnect.php
+++ dbConnect.php
@@ -5,10 +5,10 @@ $db_password = "";
 $db_name = "foods-be-like";
 
 $db_connect = mysqli_connect($db_host, $db_username, $db_password, $db_name);
-if(mysqli_connect_error())
-{
-    echo 'Failed to connect to MySQL: '.mysqli_connect_error();
-}
+
+
+
+
 
 
 ?>
