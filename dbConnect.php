cf5b6f5cb63106b3759cf1cf7af5987375c7b4ac
Iliyan <IDIliev18@codingburgas.bg>
2020-03-04 00:54:55 +0200

    Add hidden pages

--- dbConnect.php
+++ dbConnect.php
@@ -1,9 +1,8 @@
-<!-- Don't touch-->
 <?php
 $db_host = "localhost";
-$db_username = "codi7too_foodsbelike";
-$db_password = "AQ=8(+H1rJMs";
-$db_name = "codi7too_foodsbelike";
+$db_username = "";
+$db_password = "";
+$db_name = "";
 
 $db_connect = mysqli_connect($db_host, $db_username, $db_password, $db_name);
 if(mysqli_connect_error())
@@ -13,4 +12,4 @@ if(mysqli_connect_error())
 
 
 ?>
-<!-- Don't touch-->
+
