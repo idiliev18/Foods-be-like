2bc445d9afe736abcee9016f62a31bd9c3a4fda3
Iliyan <IDIliev18@codingburgas.bg>
2020-03-04 01:00:20 +0200

    remove bootstrap

--- index.php
+++ index.php
@@ -4,7 +4,6 @@
 <head>
     <link rel="stylesheet" href="CSS and Images\index.css ">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
     <link rel="icon" href="CSS and Images/Images/icon.png">
 </head>
 
