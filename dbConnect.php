7245a2c8e33213c491e8fcf6d289068535d3ec04
IDIliev18 <idiliev18@codingburgas.bg>
2020-03-14 18:51:17 +0200

    db data

--- dbConnect.php
+++ dbConnect.php
@@ -1,8 +1,8 @@
 <?php
 $db_host = "localhost";
-$db_username = "";
+$db_username = "root";
 $db_password = "";
-$db_name = "";
+$db_name = "codi7too_foodsbelike";
 
 $db_connect = mysqli_connect($db_host, $db_username, $db_password, $db_name);
 if(mysqli_connect_error())
