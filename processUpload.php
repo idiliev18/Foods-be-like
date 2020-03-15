cc239de41bf07d51901445887e31ee6da4210962
IDIliev18 <idiliev18@codingburgas.bg>
2020-03-15 21:50:24 +0200

    Add option to upload in SBI

---
+++ processUpload.php
@@ -0,0 +1,16 @@
+<?php
+include("dbConnect.php");
+
+include("dbConnect.php");
+$Name=$_REQUEST['Name'];
+$Ingradients=$_REQUEST['Ingradients'];
+$HowToMake=$_REQUEST['HowToMake'];
+$TimeToMake=$_REQUEST['TimeToMake'];
+$UploadedBy=$_REQUEST['UploadedBy'];
+
+$query=mysqli_query($db_connect,"INSERT INTO sbi (Name, Ingradients, HowToMake, TimeToMake, UploadedBy) VALUES ('$Name','$Ingradients','$HowToMake', '$TimeToMake', '$UploadedBy')") or die(mysqli_error($db_connect));
+
+mysqli_close($db_connect);
+
+
+?>
