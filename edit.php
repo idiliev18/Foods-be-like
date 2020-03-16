28ce192a1fbf6992a8410d2c22bd0df08f0949b4
IDIliev18 <idiliev18@codingburgas.bg>
2020-03-16 17:44:48 +0200

    Add delete button

--- edit.php
+++ edit.php
@@ -1,16 +1,18 @@
 <?php 
  
     require_once("dbConnect.php");
-    $UserID = $_GET['GetID'];
-    $query = " select * from records where User_ID='".$UserID."'";
+    $RecipeID = $_GET['GetID'];
+    $query = " select * from sbi where id='".$RecipeID."'";
     $result = mysqli_query($db_connect,$query);
  
     while($row=mysqli_fetch_assoc($result))
     {
-        $UserID = $row['User_ID'];
-        $UserName = $row['User_Name'];
-        $UserEmail = $row['User_Email'];
-        $UserAge = $row['User_Age'];
+        $RecipeID = $row['id'];
+        $RecipeName = $row['Name'];
+        $ingradients = $row['Ingradients'];
+        $htm = $row['HowToMake'];
+        $ttm = $row['TimeToMake'];
+        $ub = $row['UploadedBy'];
     }
  
 ?>
@@ -20,32 +22,21 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" a href="CSS/bootstrap.css"/>
-    <title>Document</title>
+    
+
 </head>
-<body class="bg-dark">
- 
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-6 m-auto">
-                    <div class="card mt-5">
-                        <div class="card-title">
-                            <h3 class="bg-success text-white text-center py-3"> Update Form in PHP</h3>
-                        </div>
-                        <div class="card-body">
- 
-                            <form action="editProcess.php?ID=<?php echo $UserID ?>" method="post">
-                                <input type="text" class="form-control mb-2" placeholder=" User Name " name="name" value="<?php echo $UserName ?>">
-                                <input type="email" class="form-control mb-2" placeholder=" User Email " name="email" value="<?php echo $UserEmail ?>">
-                                <input type="text" class="form-control mb-2" placeholder=" User Age " name="age" value="<?php echo $UserAge ?>">
-                                <button class="btn btn-primary" name="update">Update</button>
-                            </form>
- 
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
+<body>
+
+            <form action="update.php" method="POST">
+                <input type="hidden" name="id" value="<?php echo $RecipeID ?>">
+                <input type="text"  name="rname"  value="<?php echo $RecipeName ?>">
+                <input type="text" name="ingradients" value="<?php echo $ingradients ?>">
+                <input type="text"  name="htm"   value="<?php echo $htm ?>">
+                <input type="text"  name="ttm"   value="<?php echo $ttm ?>">
+                <input type="text"  name="ub"   value="<?php echo $ub ?>">
+                <input type="submit" name="act" value="Update">
+            </form>
+     
     
 </body>
 </html>
--- sbi_Edit.php
+++ sbi_Edit.php
@@ -11,8 +11,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" a href="CSS/bootstrap.css"/>
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">    
     <title>View Records</title>
 </head>
 <body>
@@ -63,10 +62,7 @@
                                    
 
                         </table>
-                    </div>
-                </div>
-            </div>
-        </div>
+
     
 </body>
 </html>
